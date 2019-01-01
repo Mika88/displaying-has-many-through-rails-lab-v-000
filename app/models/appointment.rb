a5bdc0072a07eaf3fd8656f3d628ddb1f3e81e1d
Mika88 <mikaspivak@gmail.com>
2019-01-01 02:43:00 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
 
   def readable_datetime
-    d = Date.parse(self.appointment_datetime)
+    d = DateTime.parse(self.appointment_datetime)
     d.strftime("%B %d, %Y at %I:%M")
   end
 end
