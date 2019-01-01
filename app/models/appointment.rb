05d0f7cc3b92dae8c11ba5df98486031bce55518
Mika88 <mikaspivak@gmail.com>
2019-01-01 01:52:09 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
 
   def appointment_datetime
-    t = Time.now
-    t.strftime("%B %d, %Y at %I:%M")
+    DateTime.new.strftime("%B %d, %Y at %I:%M")
   end
 end
